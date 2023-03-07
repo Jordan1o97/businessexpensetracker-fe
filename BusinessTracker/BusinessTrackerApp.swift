5a5e62e19b538a01cd5b82410b6481ea90a388ae
Jordan Davis <jorddavis@outlook.com>
2023-03-07 15:17:22 -0400

    Initial Commit

---
+++ BusinessTracker/BusinessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessTrackerApp.swift
+//  BusinessTracker
+//
+//  Created by Jordan Davis on 2023-03-07.
+//
+
+import SwiftUI
+
+@main
+struct BusinessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
